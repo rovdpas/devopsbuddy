1960ffbd2beb8fb153e55ddac6bb81dc9bca2b7e
ron <rovdpas@gmail.com>
2017-12-24 16:12:18 +0100

    Eerste opzet voor bootstrap

--- devopsbuddy/src/main/java/com/devopsbuddy/web/controllers/IndexController.java
+++ devopsbuddy/src/main/java/com/devopsbuddy/web/controllers/IndexController.java
@@ -1,14 +1,13 @@
-package com.devopsbuddy.controllers;
+package com.devopsbuddy.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//@Controller
-public class HelloWorldController {
+@Controller
+public class IndexController {
 
 	@RequestMapping("/")
-	public String sayHello(){
+	public String home(){
 		return "index";
-		
 	}
 }
