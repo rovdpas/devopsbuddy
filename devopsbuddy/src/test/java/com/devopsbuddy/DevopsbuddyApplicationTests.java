cff5c7b73ce9f6413b404544f31a21b790aa57cd
ron <rovdpas@gmail.com>
2017-12-24 20:43:41 +0100

    Mogelijkheden I18N (meertalig) toegevoegd

--- devopsbuddy/src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ devopsbuddy/src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -2,15 +2,35 @@ package com.devopsbuddy;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.devopsbuddy.web.i18n.I18NService;
+
+import org.junit.Assert;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = DevopsbuddyApplication.class)
+@WebAppConfiguration
+
 public class DevopsbuddyApplicationTests {
 
+	//@Test
+	//public void contextLoads() {
+	//}
+	
+	@Autowired
+	private I18NService i18NService;
+	
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService() throws Exception{
+		String expectedResult = "Bootstrap template van Ron";
+		String messageId="index.main.callout";
+		String actual = i18NService.getMessage(messageId);
+		Assert.assertEquals("De aangeboden en verwachtte String komen niet overeen", expectedResult, actual);
 	}
 
 }
