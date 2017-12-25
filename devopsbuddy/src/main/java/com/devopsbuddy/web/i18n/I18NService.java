e40f501084706920b01f2e0811eed5e525eb3871
ron <rovdpas@gmail.com>
2017-12-25 10:52:26 +0100

    Logging toegevoegd met Logback

--- devopsbuddy/src/main/java/com/devopsbuddy/web/i18n/I18NService.java
+++ devopsbuddy/src/main/java/com/devopsbuddy/web/i18n/I18NService.java
@@ -2,18 +2,26 @@ package com.devopsbuddy.web.i18n;
 
 import java.util.Locale;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
+import ch.qos.logback.classic.Logger;
+
 @Service
 public class I18NService {
 
+	// Application logger
+	private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(I18NService.class);
+	
+	
 	@Autowired
 	private MessageSource messageSource;
 	
 	public String getMessage(String messageId) {
+		LOG.info("RON - Geef de i18n tekst voor het messageId: {}", messageId);
 		Locale locale = LocaleContextHolder.getLocale();
 		return getMessage(messageId, locale);
 	}
